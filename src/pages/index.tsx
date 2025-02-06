a77e6ced5ac5c48cfa9510fe3d4761ec69868d08
kantaphob <64143203@g.cmru.ac.th>
2025-02-06 18:10:40 +0700

    Refactor code to add QR code images and update image slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ const personalInfoList: PersonalInfo[] = [
 export default function Home() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  
+
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -114,6 +114,39 @@ export default function Home() {
             <div className="w-24 h-1 bg-amber-600 mx-auto mb-8 transform hover:scale-x-150 transition-transform duration-300"></div>
           </section>
 
+          {/* Image POS */}
+          <section id="about" className="bg-white rounded-lg shadow-lg p-8 mb-16 transform hover:-translate-y-1 transition-all duration-300">
+            <div className="grid md:grid-cols-3 gap-8 flex-center">
+              <div className="w-64 h-64 border-4 border-amber-600 rounded-lg p-4 shadow-xl flex justify-center items-center bg-white">
+                <Image
+                  src="/imageslide/5.PNG" // Path to the QR code image
+                  alt="QR Code"
+                  width={256}
+                  height={256}
+                  className="mx-auto transition-transform duration-300 transform hover:scale-105"
+                />
+              </div>
+              <div className="w-64 h-64 border-4 border-amber-600 rounded-lg p-4 shadow-xl flex justify-center items-center bg-white">
+                <Image
+                  src="/imageslide/2.PNG" // Path to the QR code image
+                  alt="QR Code"
+                  width={256}
+                  height={256}
+                  className="mx-auto transition-transform duration-300 transform hover:scale-105"
+                />
+              </div>
+              <div className="w-64 h-64 border-4 border-amber-600 rounded-lg p-4 shadow-xl flex justify-center items-center bg-white">
+                <Image
+                  src="/imageslide/4.PNG" // Path to the QR code image
+                  alt="QR Code"
+                  width={256}
+                  height={256}
+                  className="mx-auto transition-transform duration-300 transform hover:scale-105"
+                />
+              </div>
+            </div>
+          </section>
+
           {/* Abstract Section */}
           <section id="about" className="bg-white rounded-lg shadow-lg p-8 mb-16 transform hover:-translate-y-1 transition-all duration-300">
             <h2 className="text-2xl font-semibold text-amber-800 mb-6">
@@ -135,46 +168,45 @@ export default function Home() {
                 เป็นช่องทางหลักในการสั่งซื้อ
               </p>
             </div>
-
           </section>
 
           <section id="about" className="bg-white rounded-lg shadow-lg p-8 mb-16 transform hover:-translate-y-1 transition-all duration-300">
-  <div className="grid md:grid-cols-2 gap-8">
-    {/* Image Slider */}
-    <div className="space-y-4">
-      <h2 className="text-2xl font-semibold text-amber-800 mb-4">Image Slide</h2>
-      <div className="relative w-full h-64 md:h-96 rounded-lg overflow-hidden shadow-lg">
-        <div className="flex space-x-4 overflow-x-scroll snap-x snap-mandatory">
-          {images.map((src, index) => (
-            <div key={index} className="flex-none w-full md:w-1/2 h-full snap-center">
-              <Image
-                src={src} // Path to the image
-                alt={`Image ${index + 1}`}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-lg transition-transform duration-300 transform hover:scale-105"
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+            <div className="grid md:grid-cols-2 gap-8">
+              {/* Image Slider */}
+              <div className="space-y-4">
+                <h2 className="text-2xl font-semibold text-amber-800 mb-4">Image Slide</h2>
+                <div className="relative w-full h-64 md:h-96 rounded-lg overflow-hidden shadow-lg">
+                  <div className="flex space-x-4 overflow-x-scroll snap-x snap-mandatory">
+                    {images.map((src, index) => (
+                      <div key={index} className="flex-none w-full md:w-1/2 h-full snap-center">
+                        <Image
+                          src={src} // Path to the image
+                          alt={`Image ${index + 1}`}
+                          layout="fill"
+                          objectFit="cover"
+                          className="rounded-lg transition-transform duration-300 transform hover:scale-105"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
 
-    {/* QR Code Repo */}
-    <div className="space-y-4">
-      <h2 className="text-2xl font-semibold text-amber-800 mb-4">QR Code Repo</h2>
-      <div className="w-64 h-64 border-4 border-amber-600 rounded-lg p-4 shadow-xl flex justify-center items-center bg-white">
-        <Image
-          src="/qrc/sjhb-pos67.png" // Path to the QR code image
-          alt="QR Code"
-          width={256}
-          height={256}
-          className="mx-auto transition-transform duration-300 transform hover:scale-105"
-        />
-      </div>
-    </div>
-  </div>
-</section>
+              {/* QR Code Repo */}
+              <div className="space-y-4">
+                <h2 className="text-2xl font-semibold text-amber-800 mb-4">QR Code Repo</h2>
+                <div className="w-64 h-64 border-4 border-amber-600 rounded-lg p-4 shadow-xl flex justify-center items-center bg-white">
+                  <Image
+                    src="/qrc/sjhb-pos67.png" // Path to the QR code image
+                    alt="QR Code"
+                    width={256}
+                    height={256}
+                    className="mx-auto transition-transform duration-300 transform hover:scale-105"
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
 
 
           {/* Personal Info Section */}
